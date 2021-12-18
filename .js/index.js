74d4349e8e1a8277b06544644ea5fc0453c8c6d0
carlos narea <carlosnarea22@gmail.com>
2021-12-18 20:22:19 -0300

    segunda entrega final

---
+++ .js/index.js
@@ -0,0 +1,23 @@
+window.sr =ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 3000,
+    });
+
+    sr.reveal('.main-gallery', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-5px'
+    });
+
+    sr.reveal('.main-product', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-10px'
+    });
+
+    sr.reveal('.carousel', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-10px'
+    });
